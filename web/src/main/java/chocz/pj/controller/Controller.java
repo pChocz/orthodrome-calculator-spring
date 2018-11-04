11b56dc606e0bd86d8a109a1b39fa192f14572ab
pChocz <piotr@choczynski.pl>
2018-11-04 19:45:58 +0100

    initial commit - setting up module/submodule structure, basic dependencies, adding draft of a template and basic logic of the spring webMVC approach

---
+++ web/src/main/java/chocz/pj/controller/Controller.java
@@ -0,0 +1,12 @@
+package chocz.pj.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @Autowired
+    public Controller() {
+    }
+
+}
